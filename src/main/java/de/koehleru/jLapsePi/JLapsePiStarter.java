689cc84c56435f14c0863e4d7343d55eec72a8cd
Ulrich Koehler <ukoehler@conet.de>
2014-12-11 11:55:36 +0100

    init

---
+++ src/main/java/de/koehleru/jLapsePi/JLapsePiStarter.java
@@ -0,0 +1,13 @@
+package de.koehleru.jLapsePi;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+
+public class JLapsePiStarter {
+
+	public static void main(String[] args) {
+
+		GpioController gpio = GpioFactory.getInstance();
+	}
+
+}

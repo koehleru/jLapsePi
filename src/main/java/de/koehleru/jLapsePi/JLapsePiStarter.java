fc24782e073b91ecd78f62455d4aeb1945d08f54
Ulrich Koehler <ukoehler@conet.de>
2014-12-11 16:08:21 +0100

    ...

--- src/main/java/de/koehleru/jLapsePi/JLapsePiStarter.java
+++ src/main/java/de/koehleru/jLapsePi/JLapsePiStarter.java
@@ -1,13 +1,37 @@
 package de.koehleru.jLapsePi;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
+import java.io.IOException;
 
-public class JLapsePiStarter {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class JLapsePiStarter extends Application {
 
 	public static void main(String[] args) {
-
-		GpioController gpio = GpioFactory.getInstance();
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		initLayout(arg0);
+	}
+	
+	private void initLayout(Stage arg0) {
+		try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(JLapsePiStarter.class.getResource("views/LapseOverview.fxml"));
+            AnchorPane rootLayout = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            arg0.setScene(scene);
+            arg0.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
 }

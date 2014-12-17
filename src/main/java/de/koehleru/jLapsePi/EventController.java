59d0313c210c657b90a738df74f4438d063b3169
Ulrich Koehler <ukoehler@conet.de>
2014-12-17 20:23:55 +0100

    ..

---
+++ src/main/java/de/koehleru/jLapsePi/EventController.java
@@ -0,0 +1,12 @@
+package de.koehleru.jLapsePi;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EventController {
+
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		System.exit(0);
+	}
+}
